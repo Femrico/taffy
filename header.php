fc84bf6cfaa52a9435687d03cc4bb5eeb3fb700c
Shubh Sheth <shubhsheth8@gmail.com>
2020-07-12 00:11:27 -0500

    remove default title

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
-
-    <title><?php bloginfo('name'); ?></title>
 
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
