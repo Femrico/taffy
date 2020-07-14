e17e19173a08662547f1f7429c7160f8073d1de3
Shubh Sheth <shubhsheth8@gmail.com>
2020-07-13 21:48:48 -0500

    changes to pages

--- page.php
+++ page.php
@@ -5,6 +5,7 @@ get_header();
 <main>
     <div class="col-md-10 mx-auto py-5">
         <?php while (have_posts()) : the_post(); ?>
+            <?php the_title('<h1 class="mb-4">', '</h1>'); ?>
 
             <?php the_content(); ?>
 
