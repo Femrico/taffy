8948c8bf7772bc89ab8edcd2e90b8d024ecf1386
Shubh Sheth <shubhsheth8@gmail.com>
2020-07-18 13:38:39 -0500

    adding more alt tags

--- components/nav-main.php
+++ components/nav-main.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="navbar navbar-expand-lg col-12 navbar-top">
                 <a href="/" class="navbar-brand mr-3">
-                    <img src="<?= get_template_directory_uri() ?>/assets/images/logo.svg" class="logo" loading="lazy">
+                    <img src="<?= get_template_directory_uri() ?>/assets/images/logo.svg" class="logo" loading="lazy" alt="Femrico Logo | Shop iPhone Cases and Accessories">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
