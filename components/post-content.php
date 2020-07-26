70a5f945f857ad82148859f2ce64c98ae5aa8f26
Shubh Sheth <shubhsheth8@gmail.com>
2020-07-26 13:41:04 -0500

    🐛 image size fix

--- components/post-content.php
+++ components/post-content.php
@@ -1,6 +1,6 @@
 <a class="post" href="<?= get_the_permalink(); ?>">
     <div class="bg">
-        <?php the_post_thumbnail(); ?>
+        <?php the_post_thumbnail('medium_large'); ?>
     </div>
     <div class="content">
         <h2><?php the_title(); ?></h2>
