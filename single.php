5533378e688333908eadd221b73cd388e8c351b7
Shubh Sheth <shubhsheth8@gmail.com>
2020-08-02 02:04:06 -0500

    ✏️ Fixed Container for Singles

--- single.php
+++ single.php
@@ -1,19 +1,22 @@
 <?php
 get_header();
 ?>
-
-<section class="py-5">
-    <?php while (have_posts()) : the_post(); ?>
-    <div class="post-content mx-auto">
-            <h1><?php the_title(); ?></h1>
-            <div class="post-meta">
-                Last Modified: <?php the_modified_date( 'F j, Y' ); ?>
+<main>
+    <div class="container">
+        <section class="py-5">
+            <?php while (have_posts()) : the_post(); ?>
+            <div class="post-content mx-auto">
+                    <h1><?php the_title(); ?></h1>
+                    <div class="post-meta">
+                        Last Modified: <?php the_modified_date( 'F j, Y' ); ?>
+                    </div>
+                    <?php the_post_thumbnail('full', ['class' => 'main-image']); ?>
+                    <?php the_content(); ?>
             </div>
-            <?php the_post_thumbnail('full', ['class' => 'main-image']); ?>
-            <?php the_content(); ?>
+            <?php endwhile; ?>
+        </section>
     </div>
-    <?php endwhile; ?>
-</section>
+</main>
 
 <?php
 get_footer();
