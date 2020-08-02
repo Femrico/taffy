b45e51ad1821d5691ea36c2dfb05141cacece8bf
Shubh Sheth <shubhsheth8@gmail.com>
2020-08-02 02:02:18 -0500

    ✏️ Adding containers for pages

--- page.php
+++ page.php
@@ -3,15 +3,17 @@ get_header();
 ?>
 
 <main>
-    <div class="col-md-10 mx-auto py-5">
-        <?php while (have_posts()) : the_post(); ?>
-            <?php if (!is_cart() && !is_checkout()) { ?>
-                <?php the_title('<h1 class="mb-4">', '</h1>'); ?>
-            <?php } ?>
-
-            <?php the_content(); ?>
-
-        <?php endwhile; ?>
+    <div class="container">
+        <div class="col-md-10 mx-auto py-5">
+            <?php while (have_posts()) : the_post(); ?>
+                <?php if (!is_cart() && !is_checkout()) { ?>
+                    <?php the_title('<h1 class="mb-4">', '</h1>'); ?>
+                <?php } ?>
+    
+                <?php the_content(); ?>
+    
+            <?php endwhile; ?>
+        </div>
     </div>
 </main>
 
