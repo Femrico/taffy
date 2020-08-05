cd44a8b186fd28bcfdc47df7967f349240c88691
Shubh Sheth <shubhsheth8@gmail.com>
2020-08-04 23:57:10 -0500

    📈 Added HotJar Tracking

--- footer.php
+++ footer.php
@@ -170,6 +170,7 @@
 
 <?php wp_footer(); ?>
 <script type='text/javascript'>
+    // Lucky Orange Tracking Code
     window.__lo_site_id = 232720;
 
     (function() {
@@ -180,6 +181,16 @@
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(wa, s);
     })();
+
+    // Hotjar Tracking Code
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1931943,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
 </body>
 
