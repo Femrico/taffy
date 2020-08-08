fd3db805baa91eaca96723aedd902ca84f3d6083
Shubh Sheth <shubhsheth8@gmail.com>
2020-08-08 16:21:13 -0500

    ✨ Added new design for thank you page

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Thankyou page
  *
@@ -15,73 +16,83 @@
  * @version 3.7.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
-<div class="woocommerce-order">
+<div class="woocommerce-order thankyou-page">
 
 	<?php
-	if ( $order ) :
+	if ($order) :
 
-		do_action( 'woocommerce_before_thankyou', $order->get_id() );
-		?>
+		do_action('woocommerce_before_thankyou', $order->get_id());
+	?>
 
-		<?php if ( $order->has_status( 'failed' ) ) : ?>
+		<?php if ($order->has_status('failed')) : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+			<div class="celebration-image">
+				<img src="/wp-content/themes/taffy/assets/images/emoji-broken-heart.png">
+			</div>
+			<h1>Ooops!</h1>
+
+			<p>
+				Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.
+			</p>
 
 			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
-				<?php endif; ?>
+				<a href="<?php echo esc_url($order->get_checkout_payment_url()); ?>" class="pay btn btn-primary btn-cta btn-lg mb-3"><?php esc_html_e('Try Again', 'woocommerce'); ?></a>
 			</p>
 
 		<?php else : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<div class="celebration-image">
+				<img src="/wp-content/themes/taffy/assets/images/emoji-party-popper.png">
+			</div>
+			<h1>Yaaay!</h1>
 
-			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+			<p>
+				Your order has been successfully processed and a confirmation email will be sent to <b><?= $order->get_billing_email(); ?></b> shortly.
+			</p>
 
-				<li class="woocommerce-order-overview__order order">
-					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+			<div class="order-number">
+				<p class="number"><?= $order->get_order_number(); ?></p>
+				<p class="label">Your Order Number</p>
+			</div>
 
-				<li class="woocommerce-order-overview__date date">
-					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
-					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+			<a href="/shop/" class="btn btn-primary btn-cta btn-lg mb-3">Continue Shopping</a>
 
-				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
-					<li class="woocommerce-order-overview__email email">
-						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
-						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-					</li>
-				<?php endif; ?>
+			<div class="my-5">
+				<h3 class="mb-3">Useful Links</h3>
+				<p>
+					<a href="/order-tracking/">Track Order</a> |
+					<a href="/shipping-policy/">Shipping Info</a> |
+					<a href="/faqs/">FAQs</a>
+				</p>
+			</div>
 
-				<li class="woocommerce-order-overview__total total">
-					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+			<div class="mt-5">
+				<h3 class="mb-3">Follow Us</h3>
+				<div class="social dark-bg">
+					<div class="buttons justify-content-center">
+						<a href="https://www.facebook.com/thefemrico/" target="_blank" rel="nofollow noopener" class="btn btn-social facebook"><i class="fab fa-facebook-f"></i></a>
+						<a href="#" target="_blank" rel="nofollow noopener" class="btn btn-social twitter"><i class="fab fa-twitter"></i></a>
+						<a href="https://www.instagram.com/femrico/" target="_blank" rel="nofollow noopener" class="btn btn-social instagram"><i class="fab fa-instagram"></i></a>
+						<a href="https://www.pinterest.com/femrico/boards/" target="_blank" rel="nofollow noopener" class="btn btn-social pinterest"><i class="fab fa-pinterest-p"></i></a>
+						<a href="https://www.etsy.com/shop/femrico/" target="_blank" rel="nofollow noopener" class="btn btn-social etsy"><i class="fab fa-etsy"></i></a>
+						<!-- <a href="#" target="_blank" rel="nofollow" class="btn btn-social youtube"><i class="fab fa-youtube"></i></a> -->
+					</div>
+				</div>
+			</div>
 
-				<?php if ( $order->get_payment_method_title() ) : ?>
-					<li class="woocommerce-order-overview__payment-method method">
-						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
-						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
-					</li>
-				<?php endif; ?>
-
-			</ul>
 
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
-		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
+		<?php //do_action('woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id()); ?>
+		<?php //do_action('woocommerce_thankyou', $order->get_id()); ?>
 
 	<?php else : ?>
 
-		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your order has been received.', 'woocommerce'), null); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+																										?></p>
 
 	<?php endif; ?>
 
